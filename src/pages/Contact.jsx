59ed7ff22b4e09370992fa20fe4e09fd565c7642
Abdullah <abdullahalmahmud1113@gmail.com>
2025-02-05 12:18:23 +0600

    Contact Form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ContactBanner from "./../components/contact/ContactBanner";
 import ContactForm from "./../components/home/ContactForm";
-import Address from './../components/contact/Address';
+import Address from "./../components/contact/Address";
+import { ToastContainer } from "react-toastify";
 
 const Contact = () => {
     return (
@@ -9,6 +10,18 @@ const Contact = () => {
             <ContactBanner />
             <Address />
             <ContactForm />
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </>
     );
 };
